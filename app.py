90dd62da0620d627cb30f83aa9f7492ced694919
benjygarrison <46270616+benjygarrison@users.noreply.github.com>
2022-02-24 09:55:27 -0800

    feature: create user terminal input

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-menu="""
+menu = """Please select an option
+1. Add a new journal entry
+2. View existing entries
+3. Exit journal
 
+Your selection: """
 
-"""
+welcome = "Welcome to your python journal!" 
+
+print(welcome)
+
+user_input = input(menu)
+
+while user_input := input(menu) != "3":
+    if user_input == "1":
+        print("Adding...")
+    elif user_input == "2":
+        print("Viewing...")
+    else: 
+        print("Error")        
